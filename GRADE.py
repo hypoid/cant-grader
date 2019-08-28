ba212a372293fc90808e8db2b09dba0b8cad1e94
hypoid <a.rhodes011235@gmail.com>
2019-08-28 14:21:48 -0500

    Changed the maximum allowed knots to 0 so that any knots will prevent the 5/4 grade

--- GRADE.py
+++ GRADE.py
@@ -51,8 +51,8 @@ spec2_s_ignore_knot_size_inch = 0.8
 spec2_p_ignore_knot_size_inch = 0.25
 
 spec1_s_max_knots = 6
-spec2_s_max_knots = 5
-spec2_p_max_knots = 1
+spec2_s_max_knots = 0
+spec2_p_max_knots = 0
 
 cant_min_face_filter_size = 100  # Filter to use on cant face objects
 section_length_inches = 24
